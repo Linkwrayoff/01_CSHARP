c132f75d5208425b0f47db0daebce3fe74544ae9
Artyom <linkwrayoff@gmail.com>
2024-02-14 20:21:34 +0300

    add all files

--- 01_CSHARP/Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ 01_CSHARP/Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035879722f592a9374413715b82f72b47839c1e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad39e38085bf941bfc435b11314dcf8c8787988")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
