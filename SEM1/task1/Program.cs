3ae5f8e252266a95bdae5558bd7b2a16951f8152
Artyom <linkwrayoff@gmail.com>
2024-02-20 12:55:49 +0300

    Добавил папки семинаров

---
+++ SEM1/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// int first = 25;
+// int second = 5;
+
+// if(second * second == first){
+// 	Console.WriteLine("Yes");
+// }else{
+// 	Console.WriteLine("No");
+// }
+
+// int N = 6;
+// int i = -N;
+// while (i <= N)
+// {
+// 	Console.Write(i);
+// 	Console.Write(" ");
+// 	i = i + 1;
+// }
+// int num = 123;
+// int first = num / 100;
+// int last = num % 10;
+// Console.Write("Sum first and last digit is " + (first+last));
+
+int number = 10;
+int i = 1;
+while (i <= number)
+{
+	if (i % 2 == 0)
+	{
+		Console.Write(i + " ");
+		i++;
+	}
+}
+
---
+++ SEM2/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = 21;
+int num2 = 3;
+
+if (num % num2 == 0)
+{
+	Console.Write("Четное");
+}
+else
+{
+	Console.Write(num % num2);
+}
