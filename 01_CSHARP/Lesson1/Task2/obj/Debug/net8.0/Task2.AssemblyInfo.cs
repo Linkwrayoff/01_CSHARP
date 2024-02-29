a23d8617f4ce81a4ab42740b06c55e58d38c5f9a
Artyom <linkwrayoff@gmail.com>
2024-02-29 19:38:09 +0300

    add lesson4:Task3

--- 01_CSHARP/Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ 01_CSHARP/Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c33c5e40091efed16fbc041c656f41ebffae53b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43285a86044f9bc98931709be0fd09b247f053e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 01_CSHARP/Lesson4/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// string GetLetters(string s)
+// {
+// 	string letters = "";
+// 	foreach (char e in s)
+// 	{
+// 		if (char.IsAsciiLetter(e) == true)
+// 		{
+// 			letters += e;
+// 		}
+// 	}
+// 	return letters;
+// }
+
+// Console.WriteLine("Введите символы: ");
+// string str = Console.ReadLine();
+// string result = GetLetters(str);
+// Console.WriteLine(result);
+
+
+int[] GetNumbers(string n){
+	string symbols = "";
+	foreach(char e in n){
+		
+		if(int.TryParse(char.ToString(e), out int nums) == true){
+			symbols += e;
+		}
+	}
+
+	int[] arr = new int [symbols.Length];
+	for(int i = 0; i < symbols.Length; i++){
+		int.TryParse(char.ToString(symbols[i]), out arr[i]);
+	}
+	return arr;
+}
+
+Console.WriteLine("Введите символы: ");
+string str = Console.ReadLine();
+int [] resultNums = GetNumbers(str);
+Console.WriteLine(string.Join(",", resultNums));
