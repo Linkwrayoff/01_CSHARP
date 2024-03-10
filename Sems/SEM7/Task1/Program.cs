ee88f20431787e2c1ff7ffdcad3088ba925b8295
Artyom <linkwrayoff@gmail.com>
2024-03-10 19:30:34 +0300

    add Sems:SEM7

---
+++ Sems/SEM7/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Сумма чисел через рекурсию
+
+// int SumOfDigits(int num){
+// 	if(num == 0){
+// 		return 0;
+// 	}
+// 	int sum = num % 10 + SumOfDigits(num/10);
+// 	return sum;
+// }
+
+// int num = 1007;
+// int result = SumOfDigits(num);
+// Console.Write(result);
+
+/* --------------------------------------------------- */
+
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// 	Указание =>
+// 		Использовать рекурсию. Не использовать цикл.
+// 		Пример
+// N=5 => 1 2 3 4 5
+
+void GetIntegersOfNum (int N){
+	if(N == 0){
+		return;
+	}
+	GetIntegersOfNum(N - 1);
+	Console.Write(N +" ");
+}
+
+GetIntegersOfNum(5);
---
+++ Sems/SEM7/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. Вывести на экран согласные буквы этой строки.
+
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+
+// Пример
+
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d
+
+void GetConsonants (string str){
+	if(str.Length == 0){
+		return;
+	}
+
+	string vovels = "aeoyiu";
+	if (char.IsAsciiLetter(str[0]) && !vovels.Contains(char.ToLower(str[0]))){
+		Console.Write($"{str[0]} ");
+	}
+	GetConsonants(str.Substring(1));
+}
+
+GetConsonants("asfa3f3");
